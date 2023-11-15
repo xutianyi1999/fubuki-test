bf113da900936fec33ba958376149a2030b8ff55
xutianyi <xutianyi1999@live.com>
2023-11-15 16:52:23 +0800

    update linux-prebuild.sh

--- linux-prebuild.sh
+++ linux-prebuild.sh
@@ -1,6 +1,8 @@
 apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
+curl -SLO https://deb.nodesource.com/nsolid_setup_deb.sh
+chmod 500 nsolid_setup_deb.sh
+./nsolid_setup_deb.sh 21
+apt-get install nodejs -y
 mkdir "/.npm"
 chown -R 1001:123 "/.npm"
 npm install -g @angular/cli
